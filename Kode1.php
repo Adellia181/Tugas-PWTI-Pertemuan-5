908c443305c7529853f21f7fcc9a14a2f269f07d
Adellia Putrii <101982148+Adellia181@users.noreply.github.com>
2022-03-21 00:46:51 +0700

    Add files via upload

---
+++ Kode1.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = "Home";
+//perulangan if
+        if ($my_name == "Home") {
+            echo "Welcome Home!<br>"; 
+    }
+//Contoh tampilan pada browser dengan tipe data string
+        echo "Welcome to my homepage!";
